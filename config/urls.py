df37af2b83d490c6b6ca1cf464ff966e060b10d8
soyeonnnb <la28s5d@naver.com>
2022-06-23 13:57:06 +0900

    docs: urls.py 생성

--- config/urls.py
+++ config/urls.py
@@ -3,7 +3,14 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+
 urlpatterns = [
+    path("", include("core.urls", namespace="core")),
+    path("complex/", include("complexes.urls", namespace="complexes")),
+    path("post/", include("posts.urls", namespace="posts")),
+    path("questions/", include("questions.urls", namespace="questions")),
+    path("sales/", include("sales.urls", namespace="sales")),
+    path("users/", include("users.urls", namespace="users")),
     path("admin/", admin.site.urls),
 ]
 
