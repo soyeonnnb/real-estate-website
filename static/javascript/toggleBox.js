258d5bd561bfb0844705bf6776139f4a7a4089fd
soyeonnnb <la28s5d@naver.com>
2022-07-12 13:19:18 +0900

    feat: toggle box 재사용 가능하도록 변경

--- static/javascript/toggleBox.js
+++ static/javascript/toggleBox.js
@@ -1,36 +1,30 @@
-const toggleBoxBtn = document.querySelectorAll(".manage-toggle-box");
+const toggleBoxBtns = document.querySelectorAll(".toggle-box");
 
 function removeToggleNow(){
-    const previousToggleNow = document.querySelector(".manage-toggle__now");
-    previousToggleNow.classList.remove("manage-toggle__now");
-    if (previousToggleNow.id == "manage-toggle-complex"){
-        document.querySelector("#manage-list-complex").classList.remove("manage-toggle-show");
-        document.querySelector("#manage-list-complex").className += " manage-toggle-noshow";
-    } else if (previousToggleNow.id == "manage-toggle-sale"){
-        document.querySelector("#manage-list-sale").classList.remove("manage-toggle-show");
-        document.querySelector("#manage-list-sale").className += " manage-toggle-noshow";
-    } else{
-        document.querySelector("#manage-list-question").classList.remove("manage-toggle-show");
-        document.querySelector("#manage-list-question").className += " manage-toggle-noshow";
+    const previousToggleNow = document.querySelector(".toggle__now");
+    previousToggleNow.classList.remove("toggle__now");
+    for(i=0;i<toggleBoxBtns.length;i++){
+        if (previousToggleNow.id == `toggle-${i}`){
+            document.querySelector(`#list-${i}`).classList.remove("toggle-show");
+            document.querySelector(`#list-${i}`).className += " toggle-noshow";
+            continue;
+        }
     }
 }
 
 function toggle(){
     removeToggleNow();
     const currentToggleNow = event.currentTarget;
-    currentToggleNow.className += " manage-toggle__now";
-    if (currentToggleNow.id == "manage-toggle-complex"){
-        document.querySelector("#manage-list-complex").classList.remove("manage-toggle-noshow");
-        document.querySelector("#manage-list-complex").className += " manage-toggle-show";
-    } else if (currentToggleNow.id == "manage-toggle-sale"){
-        document.querySelector("#manage-list-sale").classList.remove("manage-toggle-noshow");
-        document.querySelector("#manage-list-sale").className += " manage-toggle-show";
-    } else{
-        document.querySelector("#manage-list-question").classList.remove("manage-toggle-noshow");
-        document.querySelector("#manage-list-question").className += " manage-toggle-show";
+    currentToggleNow.className += " toggle__now";
+    for(i=0;i<toggleBoxBtns.length;i++){
+        if (currentToggleNow.id == `toggle-${i}`){
+            document.querySelector(`#list-${i}`).classList.remove("toggle-noshow");
+            document.querySelector(`#list-${i}`).className += " toggle-show";
+            continue;
+        }
     }
 }
 
-[].forEach.call(toggleBoxBtn,function(btn){ 
+[].forEach.call(toggleBoxBtns,function(btn){ 
 	btn.addEventListener("click", toggle); 
-});
+})
